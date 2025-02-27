e7f3b08132a6747b2dd3a79e400887d72141178f
Lopatkin <zedd218@gmail.com>
2025-02-27 14:05:28 +0300

    First commit

--- src/components/Chat.js
+++ src/components/Chat.js
@@ -58,7 +58,7 @@ function Chat({ userId }) {
   const messagesEndRef = useRef(null);
 
   useEffect(() => {
-    socketRef.current = io('http://localhost:4000');
+    socketRef.current = io('https://telepets.netlify.app/');
     
     socketRef.current.on('message', (msg) => {
       setMessages(prev => [...prev, msg]);

0933ec7883186cc4780cc9b1af6fc4a79610aca8
Lopatkin <zedd218@gmail.com>
2025-02-27 16:53:58 +0300

    First commit

--- server/index.js
+++ server/index.js
@@ -6,7 +6,7 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: "https://telepets.netlify.app/",
+    origin: "https://telepets.netlify.app",
     methods: ["GET", "POST"]
   }
 });

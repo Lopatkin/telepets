7b909a53d45dc197a6c0bf4c80cef9d541082a2f
Lopatkin <zedd218@gmail.com>
2025-02-28 12:33:18 +0300

    First commit

--- server/index.js
+++ server/index.js
@@ -13,7 +13,7 @@ const io = new Server(server, {
 });
 
 // Подключение к MongoDB
-mongoose.connect('mongosh "mongodb://telepetsadmin:gsdfgRGRAgHWSWJubdEfnY@e27aed3db67d.vps.myjino.ru:27017/admin"', {
+mongoose.connect('mongodb://telepetsadmin:gsdfgRGRAgHWSWJubdEfnY@e27aed3db67d.vps.myjino.ru:27017/admin', {
   useNewUrlParser: true,
   useUnifiedTopology: true
 })

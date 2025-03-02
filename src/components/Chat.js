57e9861acc5622faf04bec64af408ea8cb26c317
Lopatkin <zedd218@gmail.com>
2025-03-02 21:24:50 +0300

    interface fix - bus station pic add

--- src/components/Chat.js
+++ src/components/Chat.js
@@ -19,22 +19,8 @@ const MessagesContainer = styled.div`
   flex-direction: column;
   background: ${props => props.room === 'Автобусная остановка' 
     ? `url(${busStationImage}) no-repeat center center fixed` 
-    : '#fff'}; /* Условный фон */
+    : '#fff'}; /* Условный фон без слоя */
   background-size: cover; /* Растягиваем изображение */
-  position: relative;
-
-  /* Полупрозрачный слой для читаемости текста */
-  &::before {
-    content: '';
-    position: absolute;
-    top: 0;
-    left: 0;
-    right: 0;
-    bottom: 0;
-    background: rgba(255, 255, 255, 0.7); /* Белый слой с прозрачностью */
-    z-index: 1;
-    display: ${props => props.room === 'Автобусная остановка' ? 'block' : 'none'};
-  }
 `;
 
 const Message = styled.div`

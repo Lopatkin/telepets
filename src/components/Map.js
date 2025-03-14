43f10588c1cfee4883c8eb68e2c0f0febd76955d
Lopatkin <zedd218@gmail.com>
2025-03-14 21:20:43 +0300

    add map pic

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styled from 'styled-components';
 import foggyCityMap from '../images/foggy_city_map.jpg'; // Импортируем изображение
 

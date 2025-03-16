8faf1730d968deff432bd71954d2b045fd9b1b47
Lopatkin <zedd218@gmail.com>
2025-03-16 23:07:22 +0300

    avatars ok

--- server/index.js
+++ server/index.js
@@ -14,6 +14,7 @@ const io = new Server(server, {
   pingInterval: 25000
 });
 
+
 mongoose.connect(process.env.MONGODB_URI, {})
   .then(() => console.log('MongoDB connected'))
   .catch(err => console.error('MongoDB connection error:', err.message));

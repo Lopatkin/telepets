69735e0dbfcd00a478ec2e270ae0bd755d6c24e8
Lopatkin <zedd218@gmail.com>
2025-04-02 14:01:51 +0300

    adit inventory styles

--- src/components/Map.js
+++ src/components/Map.js
@@ -7,7 +7,7 @@ const MapContainer = styled.div`
   background: ${props => props.theme === 'dark' ? '#1A1A1A' : '#fff'};
   display: flex;
   flex-direction: column;
-  padding: 10px;
+  padding: 0px;
   box-sizing: border-box;
   position: relative;
 `;

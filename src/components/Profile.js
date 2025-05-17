a348499bace348de829ac22a3a2ef85e87ebe48c
Lopatkin <zedd218@gmail.com>
2025-05-17 23:34:57 +0300

    add ok

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import styled from 'styled-components';
 import actionsConfig from './constants/actionsConfig';
-import { rooms } from '../components/constants/rooms';
+import { rooms } from '../rooms';
 
 const ProfileContainer = styled.div`
   padding: 20px;

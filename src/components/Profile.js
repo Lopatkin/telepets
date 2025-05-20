a7ae05f4fc11802834b0a232ee3a15d1ab0fdb7e
Lopatkin <zedd218@gmail.com>
2025-05-20 15:45:05 +0300

    add freeill and diary

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -24,21 +24,7 @@ const TabButton = styled.button`
   justify-content: center;
 `;
 
-const DiaryContainer = styled.div`
-    width: 100%;
-    max-width: 300px;
-    padding: 15px;
-    background: ${props => props.theme === 'dark' ? '#2A2A2A' : '#fff'};
-    border: 1px solid ${props => props.theme === 'dark' ? '#444' : '#ddd'};
-    border-radius: 8px;
-    box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
-    margin-top: 20px;
-    display: flex;
-    flex-direction: column;
-    gap: 15px; // Увеличен отступ между элементами
-  `;
-
-  const FreeWillContainer = styled.div`
+const FreeWillContainer = styled.div`
   display: flex;
   align-items: center;
   gap: 10px;
@@ -328,7 +314,7 @@ function Profile({ user, theme, selectedTheme, telegramTheme, onThemeChange, pro
       )}
 
       {activeTab === 'diary' && (
-        <DiaryContainer>
+        <>
           <FreeWillWrapper theme={theme}>
             <FreeWillContainer>
               <SliderLabel theme={theme}>Свобода воли</SliderLabel>
@@ -351,7 +337,7 @@ function Profile({ user, theme, selectedTheme, telegramTheme, onThemeChange, pro
               <p>Логи пока недоступны</p>
             </LogContainer>
           </LogWrapper>
-        </DiaryContainer>
+        </>
       )}
 
       {activeTab === 'interface' && (

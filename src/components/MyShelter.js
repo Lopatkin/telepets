627242b61653f418c84bc7904d80d41eed53f02c
Lopatkin <zedd218@gmail.com>
2025-05-27 17:36:31 +0300

    add dwelling

--- src/components/MyShelter.js
+++ src/components/MyShelter.js
@@ -8,7 +8,7 @@ const Overlay = styled.div`
   left: 0;
   width: 100%;
   height: 100%;
-  background: rgba(0, 0, 0, 0.01); // 99% прозрачность
+  background: rgba(0, 0, 0, 0); // 100% прозрачность
   z-index: 1500; // Выше всех элементов (CloseIcon и ToggleContainer имеют z-index 1001)
   pointer-events: auto; // Учитывает клики и нажатия
 `;

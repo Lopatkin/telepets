b87e62d9cce8b3d7cf9770d1c12ab76368ea1d38
Lopatkin <zedd218@gmail.com>
2025-06-11 15:09:54 +0300

    refactor weight limits

--- src/components/constants/settings.js
+++ src/components/constants/settings.js
@@ -1,14 +1,14 @@
-export const COOLDOWN_DURATION_CONST = 10 * 100;
-export const NOTIFICATION_DURATION_CONST = 2000;
+const COOLDOWN_DURATION_CONST = 10 * 100;
+const NOTIFICATION_DURATION_CONST = 2000;
 
 // Добавляем лимиты веса
-export const WEIGHT_LIMITS = {
-    HUMAN: 40, // Лимит веса для игрока-человека (кг)
-    ANIMAL: 10, // Лимит веса для игрока-животного (кошка или собака) (кг)
-    LOCATION: 10000 // Лимит веса для любой локации (кг)
-  };
+const WEIGHT_LIMITS = {
+  HUMAN: 40, // Лимит веса для игрока-человека (кг)
+  ANIMAL: 10, // Лимит веса для игрока-животного (кошка или собака) (кг)
+  LOCATION: 10000 // Лимит веса для любой локации (кг)
+};
 
-  // Экспортируем через CommonJS
+// Экспортируем через CommonJS
 module.exports = {
   COOLDOWN_DURATION_CONST,
   NOTIFICATION_DURATION_CONST,

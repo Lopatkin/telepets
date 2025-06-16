6455d204c30175e0824b838246776a97da13521f
Lopatkin <zedd218@gmail.com>
2025-06-16 13:27:35 +0300

    refactor weight limits

--- src/components/constants/settings.js
+++ src/components/constants/settings.js
@@ -1,9 +1,11 @@
-export const COOLDOWN_DURATION_CONST = 10 * 100;
-export const NOTIFICATION_DURATION_CONST = 2000;
+const COOLDOWN_DURATION_CONST = 10 * 100;
+const INVENTORY_WEIGHT_LIMIT = {
+    human: 40,
+    animal: 20,
+    location: 1000,
+};
 
-// Добавлены константы ограничения веса для инвентарей
-export const INVENTORY_WEIGHT_LIMIT = {
-    human: 40, // Лимит веса личного инвентаря игрока-человека в кг
-    animal: 10, // Лимит веса личного инвентаря игрока-животного (кошка или собака) в кг
-    location: 10000 // Лимит веса инвентаря любой локации в кг
+module.exports = {
+    COOLDOWN_DURATION_CONST,
+    INVENTORY_WEIGHT_LIMIT,
 };

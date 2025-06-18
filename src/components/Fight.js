accdb1c4a5adae047ffcfc2948b14f1e420c4d68
Lopatkin <zedd218@gmail.com>
2025-06-18 15:20:50 +0300

    edit buttons

--- src/components/Fight.js
+++ src/components/Fight.js
@@ -293,7 +293,7 @@ function Fight({ theme, socket, user, npc, onClose, showNotification, updateUser
 
   // Функция для получения текста надписи для атаки
   const getAttackInstruction = () => {
-    return playerAttackZone ? 'Атака готова!' : 'Укажите место атаки';
+    return playerAttackZone ? 'Атака готова!' : 'Укажите атаку';
   };
 
   const replaceZoneNames = (message) => {

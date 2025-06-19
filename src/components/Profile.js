5ca2db87a9e3613f2cbc909bf34d6695eb2c8ab0
Lopatkin <zedd218@gmail.com>
2025-06-19 13:48:22 +0300

    add exp

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -123,7 +123,7 @@ const ProgressLabel = styled.span`
   font-size: 16px;
   color: ${props => props.theme === 'dark' ? '#ccc' : '#666'};
   width: 80px;
-  text-align: right;
+  text-align: left;
 `;
 
 const ProgressValue = styled.span`
@@ -389,8 +389,8 @@ function Profile({ user, theme, selectedTheme, telegramTheme, onThemeChange, pro
             <Name theme={theme}>{displayName}</Name>
           </div>
           {username && <Username theme={theme}>@{username}</Username>}
-          <ProgressBarContainer>
-            <ProgressLabel theme={theme}></ProgressLabel>
+          <ProgressBarContainer style={{ justifyContent: 'center', margin: '8px auto' }}>
+            <ProgressLabel theme={theme} style={{ width: '0' }}></ProgressLabel>
             <ProgressBar
               value={getLevelInfo(user.exp || 0).currentExp}
               max={getLevelInfo(user.exp || 0).maxExp}

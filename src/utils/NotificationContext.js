1c2219fa3bcc2857b67c4e7ec3d8f4ad73a3fc31
Lopatkin <zedd218@gmail.com>
2025-06-27 14:53:51 +0300

    add toast

--- src/utils/NotificationContext.js
+++ src/utils/NotificationContext.js
@@ -7,16 +7,13 @@ const NotificationContext = createContext();
 export const NotificationProvider = ({ children }) => {
     const showNotification = (message, type = 'info') => {
         toast[type](message, {
-            position: "center",
+            position: "top-right",
             autoClose: 2000,
             hideProgressBar: true,
             closeOnClick: true,
             pauseOnHover: true,
             draggable: true,
-            theme: 'colored',
-            style: {
-                textAlign: 'center' // Добавляем стиль для центрирования текста
-            }
+            theme: 'colored'
         });
     };
 

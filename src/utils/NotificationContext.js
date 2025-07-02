27ba98811418422df7efa8ae9e710e005a6e3977
Lopatkin <zedd218@gmail.com>
2025-07-02 11:59:34 +0300

    add toast

--- src/utils/NotificationContext.js
+++ src/utils/NotificationContext.js
@@ -19,7 +19,7 @@ export const NotificationProvider = ({ children }) => {
             </div>,
             {
                 position: "top-right",
-                autoClose: 2000,
+                autoClose: 3000,
                 hideProgressBar: true,
                 closeOnClick: true,
                 pauseOnHover: true,
